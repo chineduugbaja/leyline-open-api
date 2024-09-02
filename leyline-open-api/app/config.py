df79f72a6892277526ce40a3ca3bf594cbe0272d
chinedave <chinedu.ugbaja@useparagon.com>
2024-09-02 01:46:07 +0100

    fix failing tests on github

--- leyline-open-api/app/config.py
+++ leyline-open-api/app/config.py
@@ -3,5 +3,5 @@ import os
 # pylint: disable-next=too-few-public-methods
 class Config:
     # pylint: disable-next=line-too-long
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost:5432/mydatabase')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:password@db:5432/mydatabase')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
